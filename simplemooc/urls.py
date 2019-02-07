b3ab38fb5e598acbbb3dbd81577db41c78832171
Priscila Moreira <priscila.moreira@superbuy.com.br>
2019-02-07 12:33:48 -0200

    Adicionei a url da tela de login

--- simplemooc/urls.py
+++ simplemooc/urls.py
@@ -19,10 +19,12 @@ from simplemooc.core.views import home,contact
 from simplemooc.courses.views import index,details
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',home, name="home"),
+    path('entrar/', LoginView.as_view(template_name='accounts/login.html') , name='login' ),
     path('contato/',contact, name="contact"),
     path('cursos/',index, name="index"),
     #path('cursos/<str:pk>',details, name="details"),
@@ -30,4 +32,4 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-	urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
