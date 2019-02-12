0dc77225ea74399a14eff1df937aefbff34f0a8c
Priscila Moreira <prisciladanielem@gmail.com>
2019-02-12 21:45:55 -0200

    Adicionei as urls de logout e do painel do usu?rio

--- simplemooc/apps/accounts/urls.py
+++ simplemooc/apps/accounts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from simplemooc.apps.accounts.views import register
+from simplemooc.apps.accounts.views import register , dashboard
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('entrar/', LoginView.as_view(template_name='accounts/login.html') , name='login' ),
+    path('sair/', LogoutView.as_view(next_page='/') , name='logout' ),
     path('cadastre-se/',register, name="register"),
+    path('',dashboard, name="dashboard"),
 ]
