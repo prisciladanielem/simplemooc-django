f4a160d19c7a9a08f546a4ddde2e703853541bd0
Priscila Moreira <priscila.moreira@superbuy.com.br>
2019-02-15 12:20:25 -0200

    Criei o form para cria??o de um novo curso

--- simplemooc/apps/accounts/forms.py
+++ simplemooc/apps/accounts/forms.py
@@ -2,8 +2,11 @@ from django import forms
 
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from simplemooc.apps.courses.models import Course
 
 class RegisterForm(UserCreationForm): #Herda de UserCreationForm
+    first_name = forms.CharField(label='Primeiro nome',max_length=100)
+    last_name = forms.CharField(label='Último nome',max_length=100)
     email = forms.EmailField(label = 'E-mail')
 
     #Função para o email ser único no cadastro
@@ -17,12 +20,14 @@ class RegisterForm(UserCreationForm): #Herda de UserCreationForm
     def save(self, commit=True):
         user = super(RegisterForm, self).save(commit=False)
         user.email = self.cleaned_data['email']
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
         if commit:
             user.save()
         return user
 
 class EditAccountForm(forms.ModelForm): #ModelForm pega os campos do form
-    
+
     def clean_email(self):
         email = self.cleaned_data['email']
         queryset =  User.objects.filter(email=email).exclude(pk=self.instance.pk)
@@ -36,4 +41,10 @@ class EditAccountForm(forms.ModelForm): #ModelForm pega os campos do form
 
 
 '''Variável instance todo ModelForm tem, e ele indica que estou alterando a instância 
-atual do modelo que está sendo editado '''
+atual do modelo que está sendo editado, ou seja, o usuário logado'''
+
+class AddCoursesForm(forms.ModelForm):
+
+    class Meta:
+        model = Course
+        fields = ['name','slug','description','about','start_date', 'image']
