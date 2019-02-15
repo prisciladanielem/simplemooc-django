adc5c311b59b299f4b617287a59d74e20b3bb5fd
Priscila Moreira <priscila.moreira@superbuy.com.br>
2019-02-15 12:19:54 -0200

    Criei a url da parte de Adicionar curso

--- simplemooc/apps/accounts/urls.py
+++ simplemooc/apps/accounts/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from simplemooc.apps.accounts.views import register, dashboard, edit
+from simplemooc.apps.accounts.views import register, dashboard, edit, addCourse
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
+    path('',dashboard, name="dashboard"),
     path('entrar/', LoginView.as_view(template_name='accounts/login.html') , name='login' ),
     path('sair/', LogoutView.as_view(next_page='/') , name='logout' ),
     path('cadastre-se/',register, name="register"),
     path('editar/',edit, name="edit"),
-    path('',dashboard, name="dashboard"),
+    path('adicionarcurso/',addCourse, name="addCourse"),
 ]
