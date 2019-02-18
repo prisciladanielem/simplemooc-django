30216b36c94633a74b5402120a6034513b3798e8
Priscila Moreira <priscila.moreira@superbuy.com.br>
2019-02-18 11:59:21 -0300

    Adicionei a classe UserAdmin

--- simplemooc/apps/accounts/admin.py
+++ simplemooc/apps/accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username','name', 'email', 'data_joined'] #Mostra esses campos no admin
+    search_fields = ['name','username','email'] #Exibe uma barra de pesquisa, onde é possível pesquisar por esses campos
+
+admin.site.register(User, UserAdmin)
