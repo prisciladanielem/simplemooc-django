33d5a31f5b071133a7ca39ee5d7f5d75a3e87aac
Priscila Moreira <priscila.moreira@superbuy.com.br>
2019-02-28 11:00:41 -0300

    Adicionei o django messages para emitir alertas

--- simplemooc/apps/courses/views.py
+++ simplemooc/apps/courses/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
-from .models import Course
+from .models import Course, Enrollment
 from .forms import ContactCourse
+
 def index(request):
     courses = Course.objects.all() # carrega todas as informações do banco de dados 
     template_name = 'courses/index.html'
@@ -37,4 +40,16 @@ def details(request,slug):
     template_name = 'courses/details.html'
     return render(request, template_name, context)
 
-
+@login_required
+def enrollment(request,slug):
+    course = get_object_or_404(Course,slug=slug)
+    enrollment, created = Enrollment.objects.get_or_create( #faz um filtro.. created retorna um boolean
+    user = request.user, #pega o usuário atual
+    course = course # e cadastra no curso
+    )
+    if created: #Se a incrição foi criada
+        enrollment.active() #Altera chama a função de ativar o aluno
+        messages.success(request, 'Inscrição no curso realizada com sucesso!') #Mensagem informativa
+    else:
+        messages.info(request, 'Você já está cadastrado nesse curso!')
+    return redirect('dashboard')
