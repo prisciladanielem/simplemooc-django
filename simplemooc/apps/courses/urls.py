704265670fd62aa5ba58d1f2811f3ad24f78d0a3
Priscila Moreira <priscila.moreira@superbuy.com.br>
2019-02-28 10:58:48 -0300

    Criei a url de inscri??o no curso

--- simplemooc/apps/courses/urls.py
+++ simplemooc/apps/courses/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from simplemooc.apps.courses.views import index,details
+from simplemooc.apps.courses.views import index,details, enrollment
 
 urlpatterns = [
     path('',index, name="index"),
     path('<str:slug>/',details, name="details"),
     #path('<str:pk>',details, name="details"),
+    path('<str:slug>/incricao',enrollment, name="enrollment"),
 ]
